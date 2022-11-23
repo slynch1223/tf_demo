c84604bdaf006c9352f2baebc36fbc0fb70b8643
Sean Lynch <slynch1223@gmail.com>
2022-11-22 21:02:26 -0500

    .

--- modules/subnets/outputs.tf
+++ modules/subnets/outputs.tf
@@ -1,5 +1,5 @@
-output "arn" { value = aws_vpc.this.arn }
-output "id" { value = aws_vpc.this.id }
-output "nacl_id" { value = aws_vpc.this.default_network_acl_id }
-output "route_table_id" { value = aws_vpc.this.default_route_table_id }
-output "security_group_id" { value = aws_vpc.this.default_security_group_id }
+# output "arn" { value = aws_vpc.this.arn }
+# output "id" { value = aws_vpc.this.id }
+# output "nacl_id" { value = aws_vpc.this.default_network_acl_id }
+# output "route_table_id" { value = aws_vpc.this.default_route_table_id }
+# output "security_group_id" { value = aws_vpc.this.default_security_group_id }
